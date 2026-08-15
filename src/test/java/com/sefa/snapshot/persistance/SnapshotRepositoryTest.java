1d36845418ec4c797021d4b8d99c78d03b263019
Okkes Sefa <oksekesfackn53@gmail.com>
2026-08-15 16:29:12 +0300

    test: add snapshot repository persistence test

---
+++ src/test/java/com/sefa/snapshot/persistance/SnapshotRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.sefa.snapshot.persistance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import com.sefa.snapshot.model.FileMetadata;
+import com.sefa.snapshot.model.Snapshot;
+import com.sefa.snapshot.persistence.SnapshotRepository;
+
+public class SnapshotRepositoryTest {
+    
+    @TempDir
+    Path tempDir;
+
+    @Test
+    void shouldSaveAndLoadSnapshot() throws Exception {
+        Map<String, FileMetadata> files = new HashMap<>();
+        files.put("A.txt", new FileMetadata("A.txt",5, "111"));
+        LocalDateTime createdAt = LocalDateTime.of(2026, 8, 15, 15, 30);
+        Snapshot originalSnapshot = new Snapshot(createdAt, files);
+        SnapshotRepository repository = new SnapshotRepository();
+        Path snapshotPAth = tempDir.resolve("Snapshot.json");
+        
+        repository.save(originalSnapshot, snapshotPAth);
+        Snapshot loadedSnapshot = repository.load(snapshotPAth);
+
+        assertEquals(originalSnapshot.getCreatedAt(), loadedSnapshot.getCreatedAt());
+        assertEquals(1, loadedSnapshot.getFiles().size());
+        assertTrue(loadedSnapshot.getFiles().containsKey("A.txt"));
+        FileMetadata loadedMetadata = loadedSnapshot.getFiles().get("A.txt");
+
+        assertEquals("A.txt", loadedMetadata.getPath());
+        assertEquals(5, loadedMetadata.getSize());
+        assertEquals("111", loadedMetadata.getHash());
+        
+    }
+}
